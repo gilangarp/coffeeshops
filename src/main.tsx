55c0789b11eb837f7ec4df6e493212935c1933b3
gilangaryp <gilang2001.ap@gmail.com>
2024-07-07 16:27:36 +0700

    add header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Home from './page/Home'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/page/Layout.tsx
+++ src/page/Layout.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/layout/Header";
 
 export default function Layout() {
   return (
     <div className="container-fluid h-full w-full">
-        Layout
+        <Header/>
+        <Outlet/>
     </div>
   )
 }
